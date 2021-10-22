db8fb54ae331a2cd7b18a94d7ed771653e25753e
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-10-22 16:53:51 +0200

    Iteration#1

---
+++ iteracion1.js
@@ -0,0 +1,10 @@
+//Iteración #1: Buscar el Máximo
+
+function sum(numberOne , numberTwo) {
+    if (numberOne>numberTwo) {
+        return numberOne;
+    }else{
+        return numberTwo;
+    }
+    
+  }
