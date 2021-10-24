1dadceb3bc25fce5b291e11664b35fa15429696a
Alfonso Cifuentes <alfonsocifuentes83@gmail.com>
2021-10-25 01:35:12 +0200

    Iteration#7

---
+++ iteracion7.js
@@ -0,0 +1,22 @@
+// Iteración #7: Buscador de nombres
+
+const nameFinder = [
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(param) {
+  
+    return (nameFinder.includes(param));
+  
+}
+
+//console.log(finderName("Ricardo"));
